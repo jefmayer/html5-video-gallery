82468be48a8df85326c7ce97badda8ce52406601
Jef Mayer <modeseventyeight@gmail.com>
2014-03-27 15:22:14 -0500

    Adding core js files

---
+++ js/jquery.videogallery.js
@@ -0,0 +1,40 @@
+;(function($) {
+	
+	$.fn.videogallery = function(options) {
+		
+		var options,
+			len,
+			obj,
+			isIe = false,
+			isMobile = false,
+			isIos = false;
+		
+		this.init = function() {
+			options = obj.find('.options');
+			return this;
+		}
+		
+		function loadData() {
+			
+		}
+		
+		function parseData() {
+			
+		}
+		
+		function onGalleryItemClick() {
+			
+		}
+		
+		function detectIe() {
+			
+		}
+		
+		function detectMobile() {
+			
+		}
+		
+		return this.init();
+	}
+
+}(jQuery));
